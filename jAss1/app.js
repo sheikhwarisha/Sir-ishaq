17bbea7391a1dde9365ba8909cf17b924c5304da
sheikhwarisha <125816138+sheikhwarisha@users.noreply.github.com>
2023-04-07 09:57:24 -0700

    Add files via upload

---
+++ jAss1/app.js
@@ -0,0 +1,92 @@
+// --question1
+// let n1 = +prompt("please enter a number 1:");
+// let n2 = +prompt("please enter a number 2:");
+// if(n1>n2){
+//     console.log("number1 is greater than number2");
+// }
+// else{
+//     console.log("number2 is greater than number1");
+// }
+//-- question2
+// let n1 = +prompt("please enter any positive or negative number:");
+// if(n1>=0){
+//     console.log( n1+ " is +");
+// }
+// else{
+//     console.log( n1+ " is -");
+// }
+// question3
+// let n1 = +prompt("please enter a number 1:");
+// let n2 = +prompt("please enter a number 2:");
+// let n3 = +prompt("please enter a number 3:");
+// let n4 = +prompt("please enter a number 4:");
+// let n5 = +prompt("please enter a number 5:");
+
+// if(n1>n2 && n1>n3 && n1>n4 && n1>n5){
+//     console.log( n1+ " is larger than other numbers");
+// }
+// else if(n2>n1 && n2>n3 && n2>n4 && n2>n5){
+//     console.log( n2+ " is largerr than other numbers");
+// }
+// else if(n3>n1 && n3>n2 && n3>n4 && n3>n5){
+//     console.log( n3+ " is larger than other numbers");
+// }
+// else if(n4>n1 && n4>n2 && n4>n3 && n4>n5){
+//     console.log( n4+ " is greater than other numbers");
+// }
+// else if(n5>n1 && n5>n2 && n5>n3 && n5>n4){
+//     console.log( n5+ " is greater than other numbers");
+// }
+// else{
+//     console.log("Invalid input please try again....")
+// }
+
+// --question4
+// for (j=1;j<=15;j++){
+// if(j%2 == 0)
+//     console.log( j+ " is even");
+// else
+// console.log( j+ " is odd");
+// }
+// question5
+// let average = +prompt("please enter your marks:");
+
+// if(average <=40){
+//     console.log( average+ " your grade is F");
+// }
+// else if(average<=50){
+//     console.log( average+ " your grade is D");
+// }
+// else if(average<=60){
+//     console.log( average+ " your grade is C");
+// }
+// else if(average<=65){
+//     console.log( average+ " your grade is B");
+// }
+// else if(average<=70){
+//     console.log( average+ " your grade is A");
+// }
+// else if(average<=90){
+//     console.log( average+ " your grade is A+");
+// }
+// else if(average<=100){
+//     console.log( average+ " your grade is A++");
+// }
+
+// question6
+
+// for (j=1;j<=100;j++){
+// if(j%3 == 0 && j%5 == 0)
+//     console.log( "FIZZBUZZ");
+// elseif(j%3 == 0)
+// console.log("FIZZ");
+// elseif(j%5 == 0)
+//     console.log("BUZZ");
+// }
+// question no7
+// for(j=1 ; j>=5; j++){
+//     for(k=1 ; k<=j; k++){
+//         console.log("*");
+//     }
+//     console.log("<br>"); 
+// }
